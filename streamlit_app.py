c6973839b153a593b4c308588b00fb907d9e4809
WillsanAJantho <willsan0204@gmail.com>
2024-12-08 17:06:46 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,10 @@
 import streamlit as st
 import pandas as pd
+import spacy
+from sklearn.feature_extraction.text import TfidfVectorizer
+import nltk
+from nltk.tokenize import word_tokenize
+from nltk.stem import WordNetLemmatizer
 
 st.title('Curasense')
 
@@ -7,5 +12,6 @@ st.info('This is app predicts disease using a machine learning model!')
 
 with st.expander('Data'):
   st.write('**Raw Data**')
-  df = pd.read_csv('https://raw.githubusercontent.com/WillsanAJantho/dataset/refs/heads/main/Symptom2Disease.csv')
-  df
+  data = pd.read_csv('https://raw.githubusercontent.com/WillsanAJantho/dataset/refs/heads/main/Symptom2Disease.csv')
+  data
+

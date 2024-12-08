84dde8706dfd9a7d163fb594e5e3dce25c530e78
WillsanAJantho <willsan0204@gmail.com>
2024-12-08 16:49:20 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,5 @@ st.title('Curasense')
 
 st.info('This is app predicts disease using a machine learning model!')
 
+df = pd.read_csv('https://raw.githubusercontent.com/WillsanAJantho/dataset/refs/heads/main/Symptom2Disease.csv')
+df
